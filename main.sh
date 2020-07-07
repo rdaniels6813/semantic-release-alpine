d41cd5b26d8e9f69dd9d82edbef014f7e4b91255
Ryan Daniels <ryan.daniels@auth0.com>
2020-07-07 14:25:00 -0400

    feat: consolidate script into main.sh
    publish to dockerhub from actions

--- main.sh
+++ main.sh
@@ -1,2 +1,15 @@
 #! /bin/sh
+if [ ".npmrc" ]; then
+  cp .npmrc ~/
+fi
+if [ "$PLUGINS" != "" ]; then
+  npm install --ignore-scripts -g $PLUGINS
+fi
+if [ "~/.npmrc" ]; then
+  rm ~/.npmrc
+fi
+if [ "$PLUGINS" != "" ]; then
+  npm rebuild -g $PLUGINS
+fi
+
 semantic-release
