208c2fec9e378f00f3bb5be78ef65e38e9dafbb1
Ryan Daniels <ryan.daniels@auth0.com>
2020-11-04 10:52:03 -0500

    fix: use tee for command output

--- main.sh
+++ main.sh
@@ -11,9 +11,8 @@ fi
 if [ "$PLUGINS" != "" ]; then
   npm rebuild -g $PLUGINS
 fi
-command_response="`semantic-release`"
-echo "$command_response"
-version=`echo "$command_response" | grep "The next release version is" | rev | cut -d' ' -f1 | rev`
+semantic-release | tee semantic-release-output.txt
+version=`cat semantic-release-output.txt | grep "The next release version is" | rev | cut -d' ' -f1 | rev`
 if [ ! -z "$version" ]
 then
   echo "::set-output name=released-version::v$version"
